6324d7c46ec0095bc079fd227c172fc3e9c5f415
Jeffrey Clark <dude@zaplabs.com>
2015-01-02 23:46:43 -0600

    Fix missing global dlog and add explode helper

--- support.lua
+++ support.lua
@@ -1,5 +1,5 @@
--- Copyright (c) 2014, Jeffrey Clark. This file is licensed under the
--- Affero General Public License version 3 or later. See the COPYRIGHT file.
+-- Code in this file is not subject to the copyright or license conditions
+-- as specified in the COPYRIGHT and/or LICENSE files
 
 -- console log with optional string formatting
 log = function (...)
@@ -15,7 +15,7 @@ log = function (...)
 	end
 end
 
-debug = function (...)
+dlog = function (...)
 	if _G['debugEnabled'] then
 		log(...)
 	end
@@ -29,4 +29,39 @@ in_array = function (tb, value)
 		end
 	end
 	return false
+end
+
+-- Source: http://lua-users.org/wiki/SplitJoin
+-- Author: Lance Li
+function explode(sep, str, limit)
+    if not sep or sep == "" then return false end
+    if not str then return false end
+    limit = limit or mhuge
+    if limit == 0 or limit == 1 then return {str},1 end
+
+    local r = {}
+    local n, init = 0, 1
+
+    while true do
+        local s,e = string.find(str, sep, init, true)
+        if not s then break end
+        r[#r+1] = string.sub(str, init, s - 1)
+        init = e + 1
+        n = n + 1
+        if n == limit - 1 then break end
+    end
+
+    if init <= string.len(str) then
+        r[#r+1] = string.sub(str, init)
+    else
+        r[#r+1] = ""
+    end
+    n = n + 1
+
+    if limit < 0 then
+        for i=n, n + limit + 1, -1 do r[i] = nil end
+        n = n + limit
+    end
+
+    return r, n
 end
